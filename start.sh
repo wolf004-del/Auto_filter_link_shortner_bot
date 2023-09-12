52c1c074506e67ddbbfaad7521171b4b7c8d2eb9
wolf004-del <144588560+wolf004-del@users.noreply.github.com>
2023-09-12 14:38:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GreyMatterbots/url-auto-delete-shortener-bot /Eva
+  git clone https://github.com/wolf004-del/Auto_filter_link_shortner_bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
