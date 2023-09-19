51ac3b720f127a3dcb24d107e4c7fcfdfb210a22
wolf004-del <144588560+wolf004-del@users.noreply.github.com>
2023-09-19 19:48:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            use_mtproto=True,
+        
             workers=50,
             plugins={"root": "plugins"},
             sleep_threshold=5,
